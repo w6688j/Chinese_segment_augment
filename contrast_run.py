06f68d44f06e2e32a7230dafdd298f16d748d5ea
WJ <953372680@qq.com>
2018-10-01 01:13:59 +0800

    跑真实语料

--- contrast_run.py
+++ contrast_run.py
@@ -6,12 +6,28 @@ from selfModel.PyNLPIRModel import PyNLPIRModel
 from selfModel.StanfordCoreNLPModel import StanfordCoreNLPModel
 from selfModel.TrieModel import TrieModel
 
+
+def getTestText(dir):
+    test_text = ''
+    with open(dir, 'r', encoding='UTF-8') as f:
+        i = 1
+        lines = f.readlines()
+        for line in lines:
+            test_text += line.strip()
+            if i == 5:
+                break
+            i += 1
+    return test_text
+
+
 if __name__ == "__main__":
     # 加载停用词
     stopwords = getStopwords('data/stopword.txt')
 
     # 效果对比
-    test = '蔡英文在昨天应民进党当局的邀请，准备和陈时中一道前往世界卫生大会，和谈有关九二共识问题'
+    # test = '蔡英文在昨天应民进党当局的邀请，准备和陈时中一道前往世界卫生大会，和谈有关九二共识问题'
+    test = getTestText('E:\Projects\PyCharmProjects\Gitee\Chinese_segment_augment\data\electric.txt')
+
     print('\n======================效果对比======================')
     print('text:')
     print(test + '\n')
